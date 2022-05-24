c488da5b431cf1220f84cc9d16dadd1fcb454d0d
Alex Kiss <alex@sysrex.com>
2022-05-24 12:10:22 +0100

    add postgres

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,13 @@ import "fmt"
 // This will be responsable for the instantiaion and startup of the go app
 func Run() error {
 	fmt.Println("Start up with the error function")
+
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("Failed to connect to the database")
+		return err
+	}
+
 	return nil
 }
 
